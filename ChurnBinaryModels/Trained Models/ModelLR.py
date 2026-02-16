37dff10cd3444adde1c5146a1738c3aea58e32bb
Pradhumna-Gupta <pradhumna11gupta@gmail.com>
2026-02-16 17:14:32 +0530

    Update ModelLR.py

--- ChurnBinaryModels/Trained Models/ModelLR.py
+++ ChurnBinaryModels/Trained Models/ModelLR.py
@@ -1,10 +1,16 @@
+import os
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-lr_model = joblib.load('lr_model.pkl')
-Scaler = joblib.load('scalerLR.pkl')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, 'lr_model.pkl')
+scaler_path = os.path.join(current_dir, 'scalerLR.pkl')
+lr_model = joblib.load(model_path)
+Scaler = joblib.load(scaler_path)
 print("Model and Scaler loaded successfully!")
+data_path = os.path.join(current_dir, '..', 'data', 'churn_dataset.csv')
+Db=pd.read_csv(data_path)
 X = Db.drop('Churn_binary', axis=1)
 Y = Db['Churn_binary']
 X_train , X_test , Y_train , Y_test = train_test_split(X,Y,test_size =.2,random_state=43)
